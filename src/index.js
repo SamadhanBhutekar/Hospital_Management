80583bc6e187010af773cf9ad8e7d5ddc62165c0
SamadhanBhutekar <59012222+SamadhanBhutekar@users.noreply.github.com>
2025-02-22 13:15:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; 
 // import { HashRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
