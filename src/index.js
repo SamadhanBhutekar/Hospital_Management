b206d3197f933b89a4bea1baf778fac727e90cf4
SamadhanBhutekar <59012222+SamadhanBhutekar@users.noreply.github.com>
2025-02-22 13:06:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom"; 
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; 
+// import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,9 +10,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter> 
+    <BrowserRouter> 
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
